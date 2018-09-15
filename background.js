471609435fc12753a095d4cb3140dda36fe86807
Francisco Ruiz <fruiz500@gmail.com>
2018-09-15 11:25:07 -0500

    fixed background script going inactive
    Added some listeners to wake it up when needed. Also added to Help how to revert to no serial.

--- background.js
+++ background.js
@@ -1,7 +1,12 @@
 //deletes cached password after 5 minutes unless reset
 chrome.runtime.onMessage.addListener(
       function (request, sender, sendResponse) {
-			if(request.message == "reset_timer") resetPwdTimer()
+
+			if(request.message == "reset_timer") resetPwdTimer();			//reset timer to erase cached Master Password
+			
+			if(request.message == "start") {									//wake up if the icon is clicked
+				chrome.runtime.sendMessage({"message": "bg_here"})			//notify popup of availability
+			}
       }
 );
 
